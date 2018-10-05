e5623b6f286eaf27e6bdc468c08ca4fda056f179
Alec Luna <alecluna@csus.edu>
2018-10-04 17:43:30 -0700

    removed comments

--- Recognizer2.java
+++ Recognizer2.java
@@ -1,36 +1,3 @@
-
-// --------------------------------------------// Recognizer for simple Java
-// Class grammar// Written by JaganChidella9/3/2017// // to run on Athena
-// (linux) -// save as: JClassRecognizer.java// compile:
-// javacJClassRecognizer.java// execute: java JClassRecognizer// // EBNF Grammar
-// is -//Java Class <jClass > ::= <className> B < varlist> {<method>} E//Class
-// Name <className> ::= C|D//Variable List<varlist>::= <vardef>{,
-// <vardef>};//Variable Declaration<vardef>::=<type> <var>//Type<type> ::= I |
-// S//Variable Names<var>::= V|Z//Class Method<method> ::= P <type> <mname>
-// (<vardef>{,<vardef>}) B//<stmnt> <returnstmnt>E//Method Name<mname>::= M|N//
-// Statement<stmnt>::= <ifstmnt>|<assignstmnt>|<whilestmnt>// If
-// statement<ifstmnt>::= F <cond>T B {<stmnt>} E [L B { <stmnt>} E]// Assignment
-// Statement<assignstmnt>::= <var>= <digit>;// While Statement<whilestmnt>::= W
-// <cond>T B <stmnt>{<stmnt>} E// Condition<cond>::= (<var>== <digit>)// Return
-// Statement<returnstmnt>::= R <var>;////KEY: C and D are names of two Java
-// classes//I stands for Integer// S stands for String//B stands for { brace//E
-// stands for } brace// V and Z are names of two variables//P stands for the
-// word: public//F stands for If// T stands for Then//L stands for Else//W
-// stands for While//R stands for Return// Valid (Legal)stringstoENTERfor
-// testing class method containingfollowing // statements:ENTER the
-// corresponding string. // Response ‘Legal’validatesstring.Response ‘Errors
-// Found’invalidatesstring.// IF
-// statement:CBIV,SZ;PIM(IV)BF(V==9)TBV=8;ERV;EE$// IF THEN ELSE
-// statement:CBIV,SZ;PSM(SZ)BF(V==9)TBV=8;ELBV=0;ERV;EE$// WHILE
-// statement:CBIV,SZ;PIM(IV)BW(V==0)TBV=9;Z=2;ERV;EE$//
-// Assignment:CBIV,SZ;PIM(IV)BV=8;RV;EE$// Invalid Strings:
-// CBIV,SZ;PIM(IV)BF(V==9)TBV=8;E;RV;E$// Invalid Strings:
-// CBIV,SZ;PIM(IV)BF(V==9)TBV=8;E;RVEE$//---------------------------------------------------------------//
-// Create FIRST and FOLLOW table 70% pointsby 10/4.Showto me, get it
-// corrected.// Then Write the Parser 30% pointsby 10/9. Show to me, get it
-// corrected.// Recursivelyimprovebothforfull points. Seepage 2,3for snippet of
-// program.
-
 import java.io.*;
 import java.util.Scanner;
 
